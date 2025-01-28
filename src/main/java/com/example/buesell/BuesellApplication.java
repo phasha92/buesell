00729b2463cbcad35d1498313acae1f6bb9645c4
Pavel Bodrug <plkoij73@gmail.com>
2025-01-28 15:43:25 +0300

    Initial commit

---
+++ src/main/java/com/example/buesell/BuesellApplication.java
@@ -0,0 +1,14 @@
+package com.example.buesell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuesellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuesellApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/buesell/BuesellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buesell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuesellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
